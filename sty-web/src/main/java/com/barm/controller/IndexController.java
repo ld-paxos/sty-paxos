b5401e0e8778bec81ba493a0f9f4fd8097a7104f
ld-paxos <ddman9009@gmail.com>
2019-05-06 23:07:55 +0800

    提交一个未完成显示jsp的版本

--- sty-web/src/main/java/com/barm/controller/IndexController.java
+++ sty-web/src/main/java/com/barm/controller/IndexController.java
@@ -1,14 +1,20 @@
 package com.barm.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController
 {
     @RequestMapping("/index")
     public String index()
     {
-        return "merge index";
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login()
+    {
+        return "login";
     }
 }
