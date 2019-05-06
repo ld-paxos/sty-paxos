aef7ed4ceea98ad7e2d6a7678b1f422d4c8882c5
ld-paxos <ddman9009@gmail.com>
2019-05-06 22:02:22 +0800

    update

--- sty-web/src/main/java/com/barm/Application.java
+++ sty-web/src/main/java/com/barm/Application.java
@@ -2,19 +2,13 @@ package com.barm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application
 {
-    @RequestMapping("/")
-    public String hello()
-    {
-        return "hello";
-    }
-
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
